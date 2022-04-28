c1ab7a01dd86bfcd5ecee87ff9a0ad5c6c4ae923
AlpagaSauvage <3645@holbertonschool.com>
2022-04-28 16:40:05 +0000

    fix link

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
 chmod 777 /data/web_static/releases/test/index.html
 echo "test" >> /data/web_static/releases/test/index.html
-ln -sf /data/web_static/current /data/web_static/releases/test/
+ln -sf /data/web_static/releases/test/ /data/web_static/current 
 chown -hR ubuntu /data/
 sed -i "/listen 80 default_server;/a location /hbnb_static/ {alias /data/web_static/current/};" /etc/nginx/sites-available/default
 
