0c7283d6866c6bd303addd22c3feef858140a5f5
Omnes <kevin@streamrunners.fr>
2022-04-28 17:29:01 +0000

    Fix shellcheck issue

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,9 +5,8 @@ sudo apt update
 sudo apt -y install nginx
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
-sudo chmod 777 /data/web_static/releases/test/index.html
-sudo echo 'zizi' >> /data/web_static/releases/test/index.html
+echo 'zizi' > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
 sudo chown -Rh ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo sed -i '/server_name _;/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
