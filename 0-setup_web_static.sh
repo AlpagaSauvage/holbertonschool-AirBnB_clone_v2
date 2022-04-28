2f8009c99ca6bc1e5a5311677ee64f146341b333
Omnes <kevin@streamrunners.fr>
2022-04-28 17:35:04 +0000

    try a fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,8 @@
 
 sudo apt update
 sudo apt -y install nginx
-sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/ 
+sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
 echo 'zizi' > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
