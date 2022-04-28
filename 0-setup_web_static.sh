d2793dbb998378a668e00d98985eb700818f915e
AlpagaSauvage <3645@holbertonschool.com>
2022-04-28 16:11:08 +0000

    add task 0 and 1

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Prepare your web servers
+
+sudo apt update
+sudo apt -y install nginx
+
+mdkir -p /data/web_static/releases/test/ /data/web_static/shared/
+touch /data/web_static/releases/test/index.html
+chmod 777 /data/web_static/releases/test/index.html
+echo "test" >> /data/web_static/releases/test/index.html
+ln -sf /data/web_static/current /data/web_static/releases/test/
+chmod -R ubuntu /data/
+sed -i "/listen 80 default_server;/a location /hbnb_static/ {alias /data/web_static/current/};" /etc/nginx/sites-available/default
+
+sudo /etc/init.d/nginx restart
---
+++ 1-pack_web_static.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Compress before sending """
+
+import fabric
+import os
+from datetime import datetime
+
+def do_pack():
+    
