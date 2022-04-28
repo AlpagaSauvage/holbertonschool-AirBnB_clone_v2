3a9c23e21c8a328b09da80e9e45df109fd3b9cd5
Omnes <kevin@streamrunners.fr>
2022-04-28 17:19:56 +0000

    Add little fix to 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,6 +8,6 @@ sudo touch /data/web_static/releases/test/index.html
 sudo chmod 777 /data/web_static/releases/test/index.html
 sudo echo 'zizi' >> /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
-sudo chown -R ubuntu /data/
+sudo chown -Rh ubuntu:ubuntu /data/
 sudo sed -i '/server_name _;/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
