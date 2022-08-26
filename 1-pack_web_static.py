26a8c5cb26ba468fec4db93b8d91b225225634eb
AlpagaSauvage <3645@holbertonschool.com>
2022-08-26 09:13:53 +0000

    add pycode style

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # pack web static
 
+
 import fabric
 import os
 from datetime import datetime
@@ -8,6 +9,7 @@ dt = datetime.now()
 ts = datetime.timestamp(dt)
 date_time = datetime.fromtimestamp(ts)
 str_good = date_time.strftime("%Y%m%d%H%M%S")
+
 
 def do_pack():
     os.system('sudo tar -c "web_static_",str_good "/data/web_static/"')
